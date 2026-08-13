6ba56437eadbd3d34d4e00af5e3c1249fdaaf422
Manish Kumar <manishchauhan5535@gmail.com>
2026-08-13 13:16:32 +0530

    week 1 Q1

---
+++ BasicCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("\nAddition = " + (num1 + num2));
+        System.out.println("Subtraction = " + (num1 - num2));
+        System.out.println("Multiplication = " + (num1 * num2));
+
+        if (num2 != 0) {
+            System.out.println("Division = " + (num1 / num2));
+        } else {
+            System.out.println("Division is not possible because the second number is 0.");
+        }
+
+        sc.close();
+    }
+}
